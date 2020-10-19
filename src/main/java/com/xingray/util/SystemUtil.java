e7e20ff61f33401b4661b17fceffebcef00d4467
leixing <leixing1012@qq.com>
2020-10-19 17:46:21 +0800

    修改系统工具类

--- src/main/java/com/xingray/util/SystemUtil.java
+++ src/main/java/com/xingray/util/SystemUtil.java
@@ -1,8 +1,6 @@
 package com.xingray.util;
 
 
-import com.xingray.util.base.Result;
-
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -11,26 +9,34 @@ import java.util.Properties;
 
 public class SystemUtil {
 
-    public static Result<Object> openDirectory(String directoryPath) {
+    public static String openDirectory(String directoryPath) {
         String osName = System.getProperty("os.name").toLowerCase();
 
         if (osName.contains("windows")) {
             try {
                 Runtime.getRuntime().exec("explorer.exe " + directoryPath);
-                return Result.OK;
+                return null;
             } catch (IOException e) {
                 e.printStackTrace();
-                return new Result<>(e);
+                return e.getMessage();
             }
         } else {
-            return new Result<>("暂不支持此系统：" + osName);
+            return "暂不支持此系统：" + osName;
         }
     }
 
-    public static void property() {
+    public static String getUserDirectory() {
+        return System.getProperty("user.dir");
+    }
+
+    public static String getPath(String relativePath) {
+        return getUserDirectory() + relativePath;
+    }
+
+    public static void printSystem() {
         Runtime r = Runtime.getRuntime();
-        Properties props = System.getProperties();
-        InetAddress addr = null;
+        Properties properties = System.getProperties();
+        InetAddress addr;
         String ip = null;
         String hostName = null;
         try {
@@ -41,10 +47,11 @@ public class SystemUtil {
             e.printStackTrace();
         }
 
-        Map<String, String> map = System.getenv();
-        String userName = map.get("USERNAME");// 获取用户名
-        String computerName = map.get("COMPUTERNAME");// 获取计算机名
-        String userDomain = map.get("USERDOMAIN");// 获取计算机域名
+        Map<String, String> env = System.getenv();
+        String userName = env.get("USERNAME");// 获取用户名
+        String computerName = env.get("COMPUTERNAME");// 获取计算机名
+        String userDomain = env.get("USERDOMAIN");// 获取计算机域名
+
         System.out.println("用户名:    " + userName);
         System.out.println("计算机名:    " + computerName);
         System.out.println("计算机域名:    " + userDomain);
@@ -53,32 +60,42 @@ public class SystemUtil {
         System.out.println("JVM可以使用的总内存:    " + r.totalMemory());
         System.out.println("JVM可以使用的剩余内存:    " + r.freeMemory());
         System.out.println("JVM可以使用的处理器个数:    " + r.availableProcessors());
-        System.out.println("Java的运行环境版本：    " + props.getProperty("java.version"));
-        System.out.println("Java的运行环境供应商：    " + props.getProperty("java.vendor"));
-        System.out.println("Java供应商的URL：    " + props.getProperty("java.vendor.url"));
-        System.out.println("Java的安装路径：    " + props.getProperty("java.home"));
-        System.out.println("Java的虚拟机规范版本：    " + props.getProperty("java.vm.specification.version"));
-        System.out.println("Java的虚拟机规范供应商：    " + props.getProperty("java.vm.specification.vendor"));
-        System.out.println("Java的虚拟机规范名称：    " + props.getProperty("java.vm.specification.name"));
-        System.out.println("Java的虚拟机实现版本：    " + props.getProperty("java.vm.version"));
-        System.out.println("Java的虚拟机实现供应商：    " + props.getProperty("java.vm.vendor"));
-        System.out.println("Java的虚拟机实现名称：    " + props.getProperty("java.vm.name"));
-        System.out.println("Java运行时环境规范版本：    " + props.getProperty("java.specification.version"));
-        System.out.println("Java运行时环境规范供应商：    " + props.getProperty("java.specification.vender"));
-        System.out.println("Java运行时环境规范名称：    " + props.getProperty("java.specification.name"));
-        System.out.println("Java的类格式版本号：    " + props.getProperty("java.class.version"));
-        System.out.println("Java的类路径：    " + props.getProperty("java.class.path"));
-        System.out.println("加载库时搜索的路径列表：    " + props.getProperty("java.library.path"));
-        System.out.println("默认的临时文件路径：    " + props.getProperty("java.io.tmpdir"));
-        System.out.println("一个或多个扩展目录的路径：    " + props.getProperty("java.ext.dirs"));
-        System.out.println("操作系统的名称：    " + props.getProperty("os.name"));
-        System.out.println("操作系统的构架：    " + props.getProperty("os.arch"));
-        System.out.println("操作系统的版本：    " + props.getProperty("os.version"));
-        System.out.println("文件分隔符：    " + props.getProperty("file.separator"));
-        System.out.println("路径分隔符：    " + props.getProperty("path.separator"));
-        System.out.println("行分隔符：    " + props.getProperty("line.separator"));
-        System.out.println("用户的账户名称：    " + props.getProperty("user.name"));
-        System.out.println("用户的主目录：    " + props.getProperty("user.home"));
-        System.out.println("用户的当前工作目录：    " + props.getProperty("user.dir"));
+        System.out.println("Java的运行环境版本：    " + properties.getProperty("java.version"));
+        System.out.println("Java的运行环境供应商：    " + properties.getProperty("java.vendor"));
+        System.out.println("Java供应商的URL：    " + properties.getProperty("java.vendor.url"));
+        System.out.println("Java的安装路径：    " + properties.getProperty("java.home"));
+        System.out.println("Java的虚拟机规范版本：    " + properties.getProperty("java.vm.specification.version"));
+        System.out.println("Java的虚拟机规范供应商：    " + properties.getProperty("java.vm.specification.vendor"));
+        System.out.println("Java的虚拟机规范名称：    " + properties.getProperty("java.vm.specification.name"));
+        System.out.println("Java的虚拟机实现版本：    " + properties.getProperty("java.vm.version"));
+        System.out.println("Java的虚拟机实现供应商：    " + properties.getProperty("java.vm.vendor"));
+        System.out.println("Java的虚拟机实现名称：    " + properties.getProperty("java.vm.name"));
+        System.out.println("Java运行时环境规范版本：    " + properties.getProperty("java.specification.version"));
+        System.out.println("Java运行时环境规范供应商：    " + properties.getProperty("java.specification.vender"));
+        System.out.println("Java运行时环境规范名称：    " + properties.getProperty("java.specification.name"));
+        System.out.println("Java的类格式版本号：    " + properties.getProperty("java.class.version"));
+        System.out.println("Java的类路径：    " + properties.getProperty("java.class.path"));
+        System.out.println("加载库时搜索的路径列表：    " + properties.getProperty("java.library.path"));
+        System.out.println("默认的临时文件路径：    " + properties.getProperty("java.io.tmpdir"));
+        System.out.println("一个或多个扩展目录的路径：    " + properties.getProperty("java.ext.dirs"));
+        System.out.println("操作系统的名称：    " + properties.getProperty("os.name"));
+        System.out.println("操作系统的构架：    " + properties.getProperty("os.arch"));
+        System.out.println("操作系统的版本：    " + properties.getProperty("os.version"));
+        System.out.println("文件分隔符：    " + properties.getProperty("file.separator"));
+        System.out.println("路径分隔符：    " + properties.getProperty("path.separator"));
+        System.out.println("行分隔符：    " + properties.getProperty("line.separator"));
+        System.out.println("用户的账户名称：    " + properties.getProperty("user.name"));
+        System.out.println("用户的主目录：    " + properties.getProperty("user.home"));
+        System.out.println("用户的当前工作目录：    " + properties.getProperty("user.dir"));
+
+        System.out.println("=============   System.getProperties()   =============");
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
+        System.out.println("=============   System.getenv()   ===============");
+        for (Map.Entry<String, String> entry : env.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
     }
 }
