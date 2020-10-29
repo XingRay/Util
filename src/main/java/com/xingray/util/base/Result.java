abb11f5b9bd614fdbe6b58568f7802583469a1d2
leixing <leixing1012@qq.com>
2020-10-29 10:11:46 +0800

    更新Result

--- src/main/java/com/xingray/util/base/Result.java
+++ src/main/java/com/xingray/util/base/Result.java
@@ -5,6 +5,7 @@ public class Result<T> {
     private T date;
     private boolean success;
     private String message;
+    private int code;
     private Exception exception;
 
     public static final Result<Object> OK = new Result<>(true);
@@ -13,18 +14,18 @@ public class Result<T> {
     }
 
     public Result(boolean success) {
-        this(null, success, null, null);
+        this(null, success, null, 0, null);
     }
 
     public Result(Exception exception) {
-        this(null, false, exception.getMessage(), exception);
+        this(null, false, exception.getMessage(), 0, exception);
     }
 
     public Result(String message) {
-        this(null, false, message, null);
+        this(null, false, message, 0, null);
     }
 
-    public Result(T date, boolean success, String message, Exception exception) {
+    public Result(T date, boolean success, String message, int code, Exception exception) {
         this.date = date;
         this.success = success;
         this.message = message;
@@ -55,6 +56,14 @@ public class Result<T> {
         this.message = message;
     }
 
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
     public Exception getException() {
         return exception;
     }
@@ -69,7 +78,8 @@ public class Result<T> {
                 "date=" + date +
                 ", success=" + success +
                 ", message='" + message + '\'' +
-                ", e=" + exception +
+                ", code=" + code +
+                ", exception=" + exception +
                 '}';
     }
 }
